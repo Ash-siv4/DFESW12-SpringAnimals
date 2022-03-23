72b625b06f0d361b352e36a2e2b239c2a5089f3c
Aswene Sivaraj <ashsiva4@hotmail.co.uk>
2022-03-23 16:17:27 +0000

    track ALL FILESgit status1git status1

--- src/test/java/com/qa/animals/rest/AnimalsControllerTest.java
+++ src/test/java/com/qa/animals/rest/AnimalsControllerTest.java
@@ -1,7 +1,5 @@
 package com.qa.animals.rest;
 
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -21,53 +19,47 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.qa.animals.domain.Animals;
 
 @SpringBootTest
-@AutoConfigureMockMvc //acts like your postman - makes the test requests
-@Sql(scripts = {"classpath:animals-schema.sql","classpath:animals-data.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)// PRE-POPULATE OUT H2 DB
-@ActiveProfiles("test")//run this test class on the db in the application-test.properties
+@AutoConfigureMockMvc // act like out postman
+@Sql(scripts = { "classpath:animals-schema.sql",
+		"classpath:animals-data.sql" }, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@ActiveProfiles("test") // point to application-test.properties
 public class AnimalsControllerTest {
-	
-	@Autowired
-	private MockMvc mock; //TO MOCK THE REQUESTS
 
 	@Autowired
-	private ObjectMapper map;  //Interprets JSON
-	
+	private MockMvc mock;// to mock the requests
+
+	@Autowired
+	private ObjectMapper map; // Interprets JSON
+
 	@Test
-	void testCreate() throws Exception {
-		//--request
-		// type, url, body?
-		//body - JSON - Object
-		Animals create = new Animals("Bird",5,"M");//object
-		//convert into JSON STRING
-		String createJSON =this.map.writeValueAsString(create);
-		//build up the mock request
-		RequestBuilder mockRequest = post("/create").contentType(MediaType.APPLICATION_JSON).content(createJSON);
-		//--response
-		// body(JSON with id)
-		Animals saved = new Animals(2L, "Bird",5,"M");
-		//convert into JSON STRING
-		String savedJSON =this.map.writeValueAsString(saved);
-		//--test response is correct (status + body)
-		//status = 201 CREATED
+	void createTest() throws Exception {
+		// --request-> type, url, body? -> send
+		// body - object convert to JSON
+		Animals create = new Animals("Bird", 5, "m");
+		// convert to JSON string
+		String createJson = this.map.writeValueAsString(create);
+		// Build up the request
+		RequestBuilder mockRequest = post("/create").contentType(MediaType.APPLICATION_JSON).content(createJson);
+
+		// --response -> body and the status
+		// body - object convert to JSON
+		Animals saved = new Animals(2L, "Bird", 5, "m");
+		// convert to JSON string
+		String savedJson = this.map.writeValueAsString(saved);
+
+		// --test - check status and body
 		ResultMatcher matchStatus = status().isCreated();
-		//TEST RESPONSE BODY
-		ResultMatcher matchBody = content().json(savedJSON);
-		//perform the test
+		ResultMatcher matchBody = content().json(savedJson);
+
 		this.mock.perform(mockRequest).andExpect(matchStatus).andExpect(matchBody);
+
+		// to do it all in one line
+//		this.mock
+//				.perform(post("/create").contentType(MediaType.APPLICATION_JSON)
+//						.content(this.map.writeValueAsString(new Animals("Bird", 5, "m"))))
+//				.andExpect(status().isCreated())
+//				.andExpect(content().json(this.map.writeValueAsString(new Animals(2L, "Bird", 5, "m"))));
+
 	}
-	
-	@Test
-	void testRemove() throws Exception {
-		//--request - type-delete, path param-id, url
-		Long id = 1L;
-		RequestBuilder mockRequest = delete("/remove/?id="+id);
-		//--response - status, boolean-body
-		ResultMatcher status = status().isOk();
-		ResultMatcher body = content().string("true");
-		//---test
-		this.mock.perform(mockRequest).andExpect(body).andExpect(status);
-	}
-	
-	
-	
+
 }
--- src/test/java/com/qa/animals/service/AnimalsServiceDBTest.java
+++ src/test/java/com/qa/animals/service/AnimalsServiceDBTest.java
@@ -2,11 +2,8 @@ package com.qa.animals.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -18,124 +15,63 @@ import org.springframework.test.context.ActiveProfiles;
 import com.qa.animals.domain.Animals;
 import com.qa.animals.repo.AnimalsRepo;
 
-@SpringBootTest
-@ActiveProfiles("test")
+@SpringBootTest //lets spring know this is a test class
+@ActiveProfiles("test")//knows to connect to h2
 public class AnimalsServiceDBTest {
 
-	// data
+	@MockBean //mocks the class/interface below
+	private AnimalsRepo r;
+	
+	@Autowired //class testing
+	private AnimalsServiceDB s;
+	
 	private Animals input;
 	private Animals returned;
-
-	@Autowired // the class we are testing
-	private AnimalsServiceDB serv;
-
-	@MockBean // this is the class we are dependent on so have to mock
-	private AnimalsRepo rep;
-
+	
 	@BeforeEach
 	void setUp() {
-//		System.out.println("before");
-		input = new Animals("Reptile", 10, "Male");
-		returned = new Animals(1L, "Reptile", 10, "Male");
+		input = new Animals("reptile", 10, "m");
+		returned = new Animals(1L, "reptile", 10, "m");
 	}
-
-	@Test
-	void testCreate() {
-//		System.out.println("test 1");
-		// GIVEN
-		// input/data
-
-		// WHEN
-		Mockito.when(this.rep.save(input)).thenReturn(returned);
-		// THEN
-		assertThat(this.serv.create(input)).isEqualTo(returned);
-
-		// Verification
-		Mockito.verify(this.rep, Mockito.times(1)).save(input);
-	}
-
-	@Test
-	void testRead() {
-//		System.out.println("test 2");
-		// GIVEN
-		List<Animals> readList = new ArrayList<>();
-		readList.add(input);
-		// WHEN
-		Mockito.when(this.rep.findAll()).thenReturn(readList);
-		// THEN
-		assertThat(this.serv.read()).isEqualTo(readList);
-		// VERIFY
-		Mockito.verify(this.rep, Mockito.times(1)).findAll();
-	}
-
 	
 	
 	@Test
-	void testReadOne() {
-		// GIVEN
-		Long id = 1L;// method input
-		// variables setup in method
-		Optional<Animals> optAnim = Optional.of(returned);
-		// WHEN a certain method is called, return ....
-		Mockito.when(this.rep.findById(id)).thenReturn(optAnim);
-		// THEN check what was returned is equal to what was expected
-		assertThat(this.serv.readOne(id)).isEqualTo(returned);
-		// VERIFY
-		Mockito.verify(this.rep, Mockito.times(1)).findById(id);
+	void createTest() {
+		//GIVEN - THE DATA YOU NEED (INPUT INTO THE METHOD)
+//		Animals input = new Animals("reptile", 10, "m");
+		//WHEN -m what needs to be mocked
+		Mockito.when(this.r.save(input)).thenReturn(input);
+		//THEN - what to test
+		assertThat(this.s.create(input)).isEqualTo(input);
+		//Verification - for methods you can't test (e.g: void methods)
+		Mockito.verify(this.r, Mockito.times(1)).save(input);
 	}
 	
-
 	@Test
-	void testUpdate() {
-		// GIVEN - id, object
+	void deleteTest() {
+		//GIVEN
 		Long id = 1L;
-		// NEW ANIMAL OBJECT FOR INPUT TO UPDATE METHOD
-		Animals toUpdate = new Animals("Mammal", 8, "F");
-		// METHOD USES AN OPTIONAL VERSION OF THE ANIMAL OBJECT
+		//Optional of animal
 		Optional<Animals> opt = Optional.of(returned);
-		// UPDATED VERSION:
-		Animals updated = new Animals(id, toUpdate.getBreed(), toUpdate.getAge(), toUpdate.getGender());
-		// WHEN
-		Mockito.when(this.rep.findById(id)).thenReturn(opt);
-		Mockito.when(this.rep.save(updated)).thenReturn(updated);
-		// THEN
-		assertThat(this.serv.update(id, toUpdate)).isEqualTo(updated);
-		// VERIFY
-		Mockito.verify(this.rep, Mockito.times(1)).findById(id);
-		Mockito.verify(this.rep, Mockito.times(1)).save(updated);
+		//WHEN 
+		Mockito.when(this.r.findById(id)).thenReturn(opt);
+		//THEN
+		assertThat(this.s.delete(id)).isEqualTo(returned);
+		//verify
+		Mockito.verify(this.r, Mockito.times(1)).deleteById(id);
 	}
-
+	
 	@Test
-	void testDelete() {
-		// GIVEN
-		Long id = 1L;//user input
-		// Optional Chocolate
-		Optional<Animals> optChoco = Optional.of(returned);
-		// WHEN
-		Mockito.when(this.rep.findById(id)).thenReturn(optChoco);
-		// THEN
-		assertThat(this.serv.delete(id)).isEqualTo(returned);
-		// VERIFY
-		Mockito.verify(this.rep, Mockito.times(1)).deleteById(id);
-		Mockito.verify(this.rep, Mockito.times(1)).findById(id);
-	}
-
-	@Test
-	void testRemove() {
-		// GIVEN
+	void removeTest() {
+		//Given
 		Long id = 1L;
-		// WHEN
-		Mockito.when(this.rep.existsById(id)).thenReturn(false);
-		// THEN
-		assertThat(this.serv.remove(id)).isTrue();
-		// VERIFY
-		Mockito.verify(this.rep, Mockito.times(1)).deleteById(id);
-		Mockito.verify(this.rep, Mockito.times(1)).existsById(id);
+		//when
+		Mockito.when(this.r.existsById(id)).thenReturn(false);
+		//then
+		assertThat(this.s.remove(id)).isTrue();
+		//verify
+		Mockito.verify(this.r,Mockito.times(1)).deleteById(id);
 	}
-
-	@AfterEach
-	void clear() {
-//		System.out.println("after");
-	}
+	
 
 }
